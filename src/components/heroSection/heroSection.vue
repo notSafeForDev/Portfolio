4c71ee98f411eba03147d0d7335ef72b2f88ffc0
notSafeForDev <tobias.rheindorf@gmail.com>
2023-01-14 03:16:59 +0100

    Updated page title and favicon

--- src/components/heroSection/heroSection.vue
+++ src/components/heroSection/heroSection.vue
@@ -11,6 +11,7 @@ export default {
     <div class="header_grid">
         <div class="main_cell centered_vertically">
             <h1>Tobias Rheindorf</h1>
+            <h4>AKA notSafeForDev</h4>
             <h4>Frontend & Game Developer</h4>
         </div>
         <div>
